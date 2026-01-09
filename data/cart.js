1b08df04a9bfa49574c27bb8f47ae4a79599e771
Dawa Tenzing Sherpa <dawatenzingsherpa907@gmail.com>
2026-01-09 10:46:53 +0545

    delete product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 import { products } from "./products.js";
 
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
 },{
@@ -29,6 +29,18 @@ export function checkProductExistsInCart(id){
     
     }
   
+   
+  }
+
+
+ export function removeFromCart(productId){
+    const newCart =[];
+    cart.forEach((cartItem)=>{
+      if(productId != cartItem.productId){
+        newCart.push(cartItem);
+      }
+    });
+    cart = newCart;
    
   }
 
