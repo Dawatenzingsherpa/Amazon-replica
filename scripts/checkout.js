8c234e0c73ad5515d90459892f969cd1fe4b0cdd
Dawa Tenzing Sherpa <dawatenzingsherpa907@gmail.com>
2026-01-17 20:09:39 +0545

    make deliveryOption interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart,removeFromCart,cartQuantityAmount,updateQuantity} from "../data/cart.js";
+import { cart,removeFromCart,cartQuantityAmount,updateQuantity,updateDeliveryOption} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
@@ -40,7 +40,7 @@ cart.forEach((cartItem)=>{
 
   `
    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-      <div class="delivery-date js-delivery-date">
+      <div class="delivery-date js-delivery-date-${matchingProduct.id}">
         Delivery date: ${dateString}
       </div>
 
@@ -183,7 +183,8 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
 
     html +=
     `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option"
+    data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
       <input type="radio"
         ${isChecked  ? 'checked' : ''}
         class="delivery-option-input-${matchingProduct.id}"
@@ -205,11 +206,12 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
   return html;
 }
 
-/*document.querySelectorAll(`.delivery-option-input-e43638ce-6aa0-4b85-b27f-e1d07eb678c6`)
-  .forEach((option)=>{
-    option.addEventListener('click',()=>{
-      console.log('nice');
-      document.querySelector('.js-delivery-date')
-        .innerHTML = `Delivery date: ${dateString}`;
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+    element.addEventListener('click',()=>{
+      const {productId,deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId,deliveryOptionId);
+
+    
     })
-  });*/
+  })
