1b357dca67356587a26b45e22b83368b4e8f5704
Dawa Tenzing Sherpa <dawatenzingsherpa907@gmail.com>
2026-02-07 19:04:08 +0545

    run some automated test for updateDeliveyOption

--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { checkProductExistsInCart, cart ,loadFromStorage,removeFromCart } from "../../data/cart.js";
+import { checkProductExistsInCart, cart ,loadFromStorage,removeFromCart,updateDeliveryOption } from "../../data/cart.js";
 
 describe('testt suite: checkProductExistsInCart',()=>{
   beforeEach(()=>{
@@ -79,5 +79,33 @@ describe('test suite: removeFromCart()',()=>{
     removeFromCart('121212');
     expect(cart.length).toEqual(2);
     
+  })
+})
+
+describe('test suite: updateDeliveryOption',()=>{
+  beforeEach(()=>{
+    spyOn(localStorage,'setItem');
+  });
+
+  it('basic test',()=>{
+    
+    spyOn(localStorage,'getItem').and.callFake(()=>{
+      return JSON.stringify([{
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+        
+      }])
+    })
+    loadFromStorage();
+    updateDeliveryOption('15b6fc6f-327a-4ec4-896f-486349e85a3d','3');
+    expect(cart[0].deliveryOptionId).toEqual('3');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '3'
+        
+      }]))
   })
 })
