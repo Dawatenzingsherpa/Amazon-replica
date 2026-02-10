7fd8371cce491d4f96d35a7b64fa65be899b589d
Dawa Tenzing Sherpa <dawatenzingsherpa907@gmail.com>
2026-02-10 17:55:12 +0545

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId){
   let matchingProduct;
 
@@ -8,6 +10,32 @@ export function getProduct(productId){
   });
 
   return matchingProduct;
+}
+
+class Products{
+  id;
+  name;
+  image;
+  rating;
+  priceCents;
+  keywords;
+  
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.name = productDetails.name;
+    this.image = productDetails.image;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
+  }
+
+  getRatingUrl(){
+    return `images/ratings/rating-${this.rating.stars*10}.png`;
+  }
+
+  getPrice(){
+    return `${formatCurrency(this.priceCents)}`;
+  }
 }
 
 
@@ -670,4 +698,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Products(productDetails);
+});
+
--- test-jasmine/data/cartTest.js
+++ test-jasmine/data/cartTest.js
@@ -67,19 +67,14 @@ describe('test suite: removeFromCart()',()=>{
     
     removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
 
-    expect(cart.length).toEqual(1);
+    expect(cart.length).toEqual(0);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1,
-        deliveryOptionId: '2'
-        
-      }]))
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart','[]');
   })
 
   it('remove productId that is not in the cart',()=>{
     removeFromCart('121212');
-    expect(cart.length).toEqual(2);
+    expect(cart.length).toEqual(1);
     
   })
 })
