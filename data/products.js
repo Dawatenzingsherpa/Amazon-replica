9baab59498fce5067fa6bea883178c46cd76e0c7
Dawa Tenzing Sherpa <dawatenzingsherpa907@gmail.com>
2026-02-28 09:57:58 +0545

    convert load products from xhr into fetch

--- data/products.js
+++ data/products.js
@@ -778,6 +778,34 @@ export const products = [
 
 export let products = [];
 
+export function loadProductsFetch(){
+  const promise = fetch('https://supersimplebackend.dev/products')
+  .then((response)=>{
+    return response.json();
+  }).then((productsData)=>{
+    
+    products = productsData.map((productDetails)=>{
+      if(productDetails.type==='clothing'){
+        return new Clothing(productDetails);
+      }else if(productDetails.type==='Appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+   
+    });
+    console.log('load products');
+    
+    
+  })
+ 
+  return promise;
+}
+/*
+loadProductsFetch().then(()=>{
+  console.log('next step ');
+});
+*/
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
@@ -803,7 +831,7 @@ export function loadProducts(fun){
   xhr.send();
 }
 
-loadProducts();
+
 
 
 
